efa824c956dc0b41aee42d2bc97538015a652d26
EduardoLima100 <eduardolima100@gmail.com>
2019-03-27 23:46:15 -0300

    Inicial

---
+++ Escalonamento.c
@@ -0,0 +1,30 @@
+
+
+typedef struct T{
+	int c;
+	int p;
+	int 
+} T;
+
+void analise_u(T*tarefas){
+	int i, s = 0;
+	for(i=0, i<10;i+1){
+		s=s+(tarefas[i]*c/tarefas[i]*p);
+	}
+	if(s<"n*(2^1/N-1)"){
+
+	}
+
+}
+
+void analise_tr(){
+
+}
+
+int main(){
+	T tarefas [10];
+	//definir forma aleatória os valores de C e P(srand(); rand())
+
+	analise_u(tarefas);
+	analise_tr();
+}
