1598c303bb7954d9f7805eda2dc1b720023a44ad
EduardoLima100 <eduardolima100@gmail.com>
2019-03-28 11:24:46 -0300

    Avanço no desenvolvimento da análise

--- Escalonamento.c
+++ Escalonamento.c
@@ -1,30 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Task{
+	int prio;
+	int C;
+	int P;
+	int D = P;
+	float U = (float)C/(float)P;
+} Task;
 
-typedef struct T{
-	int c;
-	int p;
-	int 
-} T;
+void analise_u(Task *tasks){
+	int s = 0;
+	int n = sizeof(tasks)/sizeof(Task);
 
-void analise_u(T*tarefas){
-	int i, s = 0;
-	for(i=0, i<10;i+1){
-		s=s+(tarefas[i]*c/tarefas[i]*p);
+	int i;
+	for(i=0;i<n;i++){
+		s = s + tasks[i].U;
 	}
-	if(s<"n*(2^1/N-1)"){
 
+	printf("\n U = %f", s);
+
+	if(s<n*(pow(2,(float)1/n)-1)){
+		printf("Escalonamento é possível");
 	}
-
+	else{
+		printf("Possibilidade de escalonamento incerta");
+	}
 }
 
-void analise_tr(){
-
-}
+//void analise_tr(){
+//}
 
 int main(){
-	T tarefas [10];
+	printf("Main")
+	Task tasks[10];
 	//definir forma aleatória os valores de C e P(srand(); rand())
 
-	analise_u(tarefas);
-	analise_tr();
+	analise_u(tasks);
+	//analise_tr();
 }
