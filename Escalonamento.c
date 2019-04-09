9238274f8a2f6fe1771c683eee0bbca0a4d6f726
EduardoLima100 <eduardolima100@gmail.com>
2019-04-08 22:24:09 -0300

    Finish

--- Escalonamento.c
+++ Escalonamento.c
@@ -5,15 +5,15 @@
 #define N_TASKS 3
 
 typedef struct Task{
-	int prio;
+	int prio;	//Prioridade
 	int C;
-	int P;
+	int P;		//Período
 	int D;
 	float U;
 } Task;
 
-void analise_u(Task *tasks,float n){
-	printf("\n\nAnálise de Utilização");
+void analise_u(Task *tasks, float n){
+	printf("\n\nAnálise por Utilização");
 	float s = 0;
 
 	int i;
@@ -35,22 +35,55 @@ void analise_u(Task *tasks,float n){
 	}
 }
 
-/*
-void analise_tr(){
+void analise_tr(Task *tasks, float n){
+	printf("\n\nAnálise por Tempo de Resposta");
+	float Ci, Cj;
+	float Pi, Pj;
+	float Rant, R;
+	float I;
+	int i;
+	int j;
+
+	i = 0;
+	while(j<n){
+		Ci = tasks[i].C;
+		Pi = tasks[i].P;
+
+		Rant = 0;
+		R = Ci;
+		while(!(Rant == R) && R<Pi && i>0){
+			Rant = R;
+			I = 0;
+			for(j=0; j<i;j++){
+				Cj = tasks[j].C;
+				Pj = tasks[j].P;
+				I = I + ceil(Rant/Pj)*Cj;
+			}
+			R = Ci + I;
+		}
+		i = i + 1;
+	}
+
+
+	if(R>=Pi){
+		printf("\nEscalonamento não é possível\n");
+	}
+	else{
+		printf("\nEscalonamento é possível\n");
+	}
 }
-*/
 
 int main(){
 	srand(time(0));
 
 	int n = N_TASKS;
-	Task tasks[n];
-	//definir forma aleatória os valores de C e P(srand(); rand())
-
+	Task tasks[n], tasks_prio[n];
+	
 	int i;
 	for(i=0;i<n;i++){
-		tasks[i].P = (rand() % 500);
-		tasks[i].C = (rand() % 100);
+		tasks[i].prio = 0;
+		tasks[i].P = rand() % 500;
+		tasks[i].C = rand() % 100;
 		tasks[i].prio = i;
 		tasks[i].D = tasks[i].P;
 		tasks[i].U = (float)tasks[i].C/(float)tasks[i].P;
@@ -60,5 +93,24 @@ int main(){
 	}
 
 	analise_u(tasks,n);
-	//analise_tr();
+		
+	int j = 0;
+  	int men = 0;
+  	for (i = 0;i<n;i++){
+    	while(j<(n-1)){
+      		if (tasks[men].P >= tasks[j+1].P){
+        		men = j+1;
+      		}
+      		j++;
+    	}
+    	tasks[men].prio = i+1;
+   		tasks_prio[i] = tasks[men];
+    	tasks[men].P = 500*n;
+    	j = -1;
+  	}
+	for(i=0;i<n;i++){
+		printf("prio[%i] = %i\n", i, tasks[i].prio);
+	}
+
+	analise_tr(tasks,n);
 }
