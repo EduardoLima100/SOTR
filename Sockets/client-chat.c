28cad4b8f270c6844f2db64381c1591fc2888d56
EduardoLima100 <eduardolima100@gmail.com>
2019-05-11 20:02:09 -0300

    Update client-chat.c

--- Sockets/client-chat.c
+++ Sockets/client-chat.c
@@ -18,7 +18,7 @@ void *leitura(void *arg) {
         bzero(buffer,sizeof(buffer));
         n = recv(sockfd,buffer,50,0);
         if (n <= 0) {
-            printf("Erro lendo do socket!\n");
+            printf("\nErro lendo do socket!\n");
             exit(1);
         }
         printf("\nMSG: %s\n",buffer);
@@ -32,13 +32,13 @@ int main(int argc, char *argv[]) {
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"Uso: %s nomehost porta\n", argv[0]);
+       fprintf(stderr,"\nUso: %s nomehost porta\n", argv[0]);
        exit(0);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        printf("Erro criando socket!\n");
+        printf("\nErro criando socket!\n");
         return -1;
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -47,17 +47,17 @@ int main(int argc, char *argv[]) {
     inet_aton(argv[1], &serv_addr.sin_addr);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
-        printf("Erro conectando!\n");
+        printf("\nErro conectando!\n");
         return -1;
     }
     pthread_create(&t, NULL, leitura, NULL);
     do {
         bzero(buffer,sizeof(buffer));
-        printf("Digite a mensagem (ou sair):");
+        printf("\nDigite a mensagem (ou sair):");
         fgets(buffer,50,stdin);
         n = send(sockfd,buffer,50,0);
         if (n == -1) {
-            printf("Erro escrevendo no socket!\n");
+            printf("\nErro escrevendo no socket!\n");
             return -1;
         }
         if (strcmp(buffer,"sair\n") == 0) {
